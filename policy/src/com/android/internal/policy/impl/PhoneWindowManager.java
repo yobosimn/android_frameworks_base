3ff05d684994a197488984e18dda5da1ac2ac9f3
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-07-28 01:01:54 +0100

    Disable Google's home-key handling code
    This might be temporary. Upstream now includes multi-behavior key
    handling for HOME, which conflicts with our own code when running
    in parallel, resulting in funky behaviors. Since the CM implementation
    is more flexible, let's keep that for the time being and disable
    Google's; It might be a good idea to rewrite our version so that it's
    an extension of theirs, though.
    
    Change-Id: I5d55301772607315b410880c1194a0076e21cb80

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1118,6 +1118,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      * eg. Disable long press on home goes to recents on sw600dp.
      */
     private void readConfigurationDependentBehaviors() {
+        /* TEMP DISABLE: This conflicts with CM's own multi-behavior code 
         mLongPressOnHomeBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnHomeBehavior);
         if (mLongPressOnHomeBehavior < LONG_PRESS_HOME_NOTHING ||
@@ -1131,6 +1132,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mDoubleTapOnHomeBehavior > DOUBLE_TAP_HOME_RECENT_SYSTEM_UI) {
             mDoubleTapOnHomeBehavior = LONG_PRESS_HOME_NOTHING;
         }
+        */
     }
 
     @Override
@@ -2164,6 +2166,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Log.w(TAG, "RemoteException from getPhoneInterface()", ex);
                 }
 
+                /* TEMP DISABLE: This conflicts with CM's own multi-behavior code 
                 // Delay handling home if a double-tap is possible.
                 if (mDoubleTapOnHomeBehavior != DOUBLE_TAP_HOME_NOTHING) {
                     mHandler.removeCallbacks(mHomeDoubleTapTimeoutRunnable); // just in case
@@ -2176,6 +2179,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // Go home!
                 launchHomeFromHotKey();
                 return -1;
+                */
             }
 
             // If a system window has focus, then it doesn't make sense
