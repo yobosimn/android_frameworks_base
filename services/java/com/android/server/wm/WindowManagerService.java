c5b6740cef6fe6d394c4c8d3ddddfad3c38daa25
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-09-01 18:27:52 +0300

    PhoneWindowManager: Delay setting updates during window animation.
    Change-Id: I90ee5d8f83176a38a32b5ff38c4b191f1b78532e

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -9563,6 +9563,7 @@ public class WindowManagerService extends IWindowManager.Stub
     void scheduleAnimationLocked() {
         if (!mAnimationScheduled) {
             mAnimationScheduled = true;
+            mPolicy.windowAnimationStarted();
             mChoreographer.postCallback(
                     Choreographer.CALLBACK_ANIMATION, mAnimator.mAnimationRunnable, null);
         }
