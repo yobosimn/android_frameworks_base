78c0cba02c51fd66874d3851dce7711c32a1ba9d
nocoast <voteforpedro8@gmail.com>
2013-11-22 04:54:13 -0600

    Revert "Bluetooth: DUN: Add framework changes to support DUN"
    This reverts commit 61eb5b5b2c7bc4e480f5d58ac84a9c141b1b880b.

--- core/java/android/bluetooth/BluetoothProfile.java
+++ core/java/android/bluetooth/BluetoothProfile.java
@@ -110,12 +110,6 @@ public interface BluetoothProfile {
     public static final int SAP = 20;
 
     /**
-     * DUN
-     * @hide
-     */
-    public static final int DUN = 21;
-
-    /**
      * Default priority for devices that we try to auto-connect to and
      * and allow incoming connections for the profile
      * @hide
